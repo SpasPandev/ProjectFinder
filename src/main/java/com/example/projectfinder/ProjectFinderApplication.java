a2b329862b71dcb91ffdc00a4a0d87d29978b99c
SpasPandev <sppan@abv.bg>
2022-08-17 12:40:20 +0300

    Init

---
+++ src/main/java/com/example/projectfinder/ProjectFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFinderApplication.class, args);
+    }
+
+}
